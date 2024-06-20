b557c477202942fddd16f65f18675e2443976567
verisify <165027751+verisify@users.noreply.github.com>
2024-06-20 13:07:06 +0100

    Delete my_streamlit_app/app.py

--- my_streamlit_app/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# Load your HTML content
-with open("my_streamlit_app/alert.html", "r") as file:
-    html_content = file.read()
-
-# Use an iframe to display the HTML content
-st.title("Success Alert Example")
-st.components.v1.html(html_content, width=None, height=400, scrolling=True)
